a0a65fd07aac5de723d8de6d4cbd34b571013ae7
purrnama <purrnama@proton.me>
2023-09-20 22:30:42 +0800

    client send name and proof to server to verify

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  const name = "Tricia Walker";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log(root);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
+    root: root,
   });
 
   console.log({ gift });
